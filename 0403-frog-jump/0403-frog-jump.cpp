fda7e8a6365412f6af859ed14e2226b646bb528f
Anandita Raj <anandita.raj27@gmail.com>
2025-08-24 03:30:58 +0530

    Time: 61 ms (78.27%), Space: 23 MB (96.04%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool dfs(int p, int j, vector<int>& s, map<int,int>& mp, map<pair<int,int>,bool>& dp) {
+        if (p == s.back()) 
+        return 1;
+
+        if (dp.count({p,j})) 
+        return dp[{p,j}];
+
+        for (int k = j-1; k <= j+1; k++) {
+            if (k > 0 && mp.count(p+k)) 
+                if (dfs(p+k, k, s, mp, dp)) return dp[{p,j}] = 1;
+            
+        }
+        return dp[{p,j}] = 0;
+    }
+
+    bool canCross(vector<int>& s) {
+        map<int,int> mp;
+        
+        for (int x : s) 
+        mp[x] = 1;
+
+        map<pair<int,int>,bool> dp;
+        return dfs(0, 0, s, mp, dp);
+    }
+};
