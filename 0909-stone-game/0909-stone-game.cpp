52c0bdd99d54fb4bc35710bfb5d26b960e221a0c
Anandita Raj <anandita.raj27@gmail.com>
2025-08-31 17:50:15 +0530

    Time: 0 ms (100%), Space: 10.4 MB (86.97%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -1,27 +1,6 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& p) {
-        int n = p.size(), maxi, mini, a=0,b=0;
-
-        // for(int i =0; i<n; i++)
-        // {
-        //     maxi = max(p[i],p[n-i-1]);
-        //     mini = min(p[i],p[n-i-1]);
-        //     a+=maxi;
-        //     b+=mini;
-
-        //    int l  = i;
-        //    int r = n - i - 1;
-
-        //    if (l<r) {
-        //    p.erase(p.begin() + r); 
-        //    p.erase(p.begin() + l); 
-        //    } 
-    
-
-        // }
-        // cout<< a<<" "<<b;
-        // return (a>b)? true:false;
         return true;
         
     }
