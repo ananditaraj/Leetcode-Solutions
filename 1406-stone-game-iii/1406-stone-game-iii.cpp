142ff6ccea3c06acc19b5d559eff24a1ef41769e
Anandita Raj <anandita.raj27@gmail.com>
2026-08-03 22:53:57 +0530

    Time: 12 ms (86.94%), Space: 136.2 MB (65.5%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string stoneGameIII(vector<int>& sv) {
+        int n=sv.size();
+        vector<int> dp(n+1, 0);
+
+        for (int i=n-1; i>=0; i--) {
+            int ans=INT_MIN,s=0;
+
+            for (int k=1;k<=3;k++) 
+            {
+                if (i+k>n) break;
+
+                s+=sv[i+k-1];
+                ans=max(ans, s-dp[i+k]);
+            }
+            dp[i]=ans;
+        }
+        return dp[0]>0?"Alice":(dp[0]<0?"Bob":"Tie");
+    }
+};
